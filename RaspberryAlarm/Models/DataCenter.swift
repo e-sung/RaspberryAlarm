c7a549e3ef158a268bd31842d4c484807c993a77
류성두 <lyuseongdu@lyuseongduui-MacBook-Air.local>
2017-11-13 12:10:03 +0900

    Add sorting feature on function alarmsOfDay Before : alarmsOfDay(with offSet:Int) After : alarmsOfDay(with offSet:Int, shouldSort:Bool)

--- RaspberryAlarm/Models/DataCenter.swift
+++ RaspberryAlarm/Models/DataCenter.swift
@@ -12,17 +12,35 @@ class DataCenter{
     static var main:DataCenter = DataCenter()
     let defaultAlarm = AlarmItem((6,30))!
     var alarmItems:[AlarmItem] = []
+//    var nearestAlarm:AlarmItem{
+//        get{
+//            var nearestAlarmOnToday:[AlarmItem] = []
+//            var alarmsOfToday = alarmsOfDay(with: 0)
+//
+//        }
+//    }
     
     /**
-    AlarmItems of a day that user specified with parameter
-     - parameter offSet: if 0, it means today. if 1, it means tomorrow
+     AlarmItems of a day that user specified with offSet
+     
+     ````
+     let todayAlarms = alarmsOfDay(with 0)
+     let tomorrowAlarms = alarmsOfDay(with 1)
+     ````
      */
-    func alarmsOfDay(with offSet:Int, )->[AlarmItem]{
+    private func alarmsOfDay(with offSet:Int, shouldSort:Bool)->[AlarmItem]{
         var alarmsToReturn:[AlarmItem] = []
         let day = (Calendar.current.component(.weekday, from: Date()) + 1)%7
         for item in alarmItems{
             if item.repeatDays.contains(Day(rawValue: day)!){
                 alarmsToReturn.append(item)
+            }
+        }
+        if shouldSort {
+            alarmsToReturn.sort { (item1, item2) -> Bool in
+                let wt1 = item1.timeToWakeUp.0*60 + item1.timeToWakeUp.1
+                let wt2 = item2.timeToWakeUp.0*60 + item2.timeToWakeUp.1
+                return wt1 < wt2
             }
         }
         return alarmsToReturn
