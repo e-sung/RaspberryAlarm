25176a6b9c8d676420c4a36e1896257135de98bc
e-sung <dev.esung@gmail.com>
2017-11-16 19:51:04 +0900

    Add Commetns on Days.swift

--- RaspberryAlarm/Models/Day.swift
+++ RaspberryAlarm/Models/Day.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+/**
+월화수목금토일
+ - Note :
+ DateComponents(.day)도 Sunday를 '1'로 잡음
+ 특별한 이유가 있지 않으면, 여기에 설정된 rawValue를 바꾸지 말 것
+ */
 enum Day:Int{
     case Sun = 1
     case Mon = 2
