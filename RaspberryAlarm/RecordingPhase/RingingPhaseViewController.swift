95eee7fc2cb8905a9e7f23bff52c33d9b8d0aefd
e-sung <dev.esung@gmail.com>
2017-11-17 22:16:45 +0900

    Encapsulate RingingPhaseVC

--- RaspberryAlarm/RecordingPhase/RingingPhaseViewController.swift
+++ RaspberryAlarm/RecordingPhase/RingingPhaseViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 import AVFoundation
 class RingingPhaseViewController: UIViewController {
 
-    var player:AVPlayer?
-    @IBAction func snoozeButtonHandler(_ sender: UIButton) {
+    private var player:AVPlayer?
+    @IBAction private func snoozeButtonHandler(_ sender: UIButton) {
         performSegue(withIdentifier: "unwindToRecordingPhase", sender: Phase.snooze)
     }
     
-    @IBAction func terminateButtonHandler(_ sender: UIButton) {
+    @IBAction private func terminateButtonHandler(_ sender: UIButton) {
         self.performSegue(withIdentifier: "unwindToAlarmList", sender: nil)
         let url = URL(string: "http://192.168.0.20:3030")!
         URLSession.shared.dataTask(with: url) { (data, response, error) in
