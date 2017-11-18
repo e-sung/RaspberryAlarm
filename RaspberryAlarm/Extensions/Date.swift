1d27d106b2c6df520f8222cae9ba1ae0bc555965
e-sung <dev.esung@gmail.com>
2017-11-18 18:03:52 +0900

    Add Comments on Date.swift

--- RaspberryAlarm/Extensions/Date.swift
+++ RaspberryAlarm/Extensions/Date.swift
@@ -10,6 +10,18 @@ import Foundation
 
 extension Date{
     static let mainDateFormat = "HH:mm:ss"
+    /**
+    주어진 Date객체가 속한 날의 0시0분0초 부터 Date객체 본인까지의 Interval
+     ````
+     오늘자정.absoluteSeconds == 0
+     내일자정.absoluteSeconds == 0
+     오늘정오.absoluteSeconds == 12*60*60
+     내일정오.absoluteSeconds == 12*60*60
+     ````
+     - ToDo :
+     TimeInterval은 Double 타입으로 만들어져있음.
+     '초'단위는 전부 정수형으로 만들었는데, 다 Double로 바꿔야 할 듯
+    */
     var absoluteSeconds:Int{
         get{
             let hour = Calendar.current.component(.hour, from: self)
@@ -19,7 +31,11 @@ extension Date{
         }
     }
     /**
-     초단위의 시간을 넣으면 "HH:mm:ss" 형식의 문자열 반환
+     초단위의 시간을 넣으면 dateFormat 형식의 문자열 반환
+     - parameter seconds : 0~59 사이의 실수
+     - parameter dateFormat : "HH:mm:ss"등, [ISO8601](https://ko.wikipedia.org/wiki/ISO_8601) 표준을 따르는 형식
+     - ToDo :
+     seconds를 Double로 만들어야 함
      */
     static func format(seconds:Int, with dateFormat:String)->String{
         let formatter = DateFormatter(); formatter.dateFormat = dateFormat
