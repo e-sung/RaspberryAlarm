afce8ad196f263d787872c007965e1a3582ae74a
e-sung <dev.esung@gmail.com>
2017-11-21 14:13:12 +0900

    Save current set time to UserDefaults.standard

--- RaspberryAlarm/MainView/MainViewController.swift
+++ RaspberryAlarm/MainView/MainViewController.swift
@@ -11,10 +11,24 @@ import HGCircularSlider
 
 class MainViewController: UIViewController {
     @IBOutlet weak var hourIndicatingSlider: CircularSlider!
-    
     @IBOutlet weak var minuteIndicatingSlider: CircularSlider!
+    
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var minuteLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if UserDefaults.standard.object(forKey: "wakeUpHour") == nil {
+            hourIndicatingSlider.endPointValue = 6.0
+            minuteIndicatingSlider.endPointValue = 30.0
+            hourLabel.text = "06:"
+            minuteLabel.text = "30"
+        }else{
+            hourIndicatingSlider.endPointValue = CGFloat(UserDefaults.standard.integer(forKey: "wakeUpHour"))
+            minuteIndicatingSlider.endPointValue = CGFloat(UserDefaults.standard.integer(forKey: "wakeUpMinute"))
+            hourLabel.text = "\(UserDefaults.standard.integer(forKey: "wakeUpHour")):"
+            minuteLabel.text = "\(UserDefaults.standard.integer(forKey: "wakeUpMinute"))"
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -24,7 +38,18 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func hourChangeHandler(_ sender: CircularSlider) {
+        let hour = Int(sender.endPointValue)
+        hourLabel.text = hour < 10 ? "0\(hour):" : "\(hour):"
+        UserDefaults.standard.set(hour, forKey: "wakeUpHour")
+    }
+    
+    @IBAction func minuteChangeHandler(_ sender: CircularSlider) {
+        let minute = Int(sender.endPointValue)
+        minuteLabel.text = minute < 10 ? "0\(minute)" : "\(minute)"
+        UserDefaults.standard.set(minute, forKey: "wakeUpMinute")
+    }
+    
     /*
     // MARK: - Navigation
 
