374f03855f47a790379f13a8558498857115cbe2
e-sung <dev.esung@gmail.com>
2017-11-21 14:38:09 +0900

    Same Logic. just refactored

--- RaspberryAlarm/MainView/MainViewController.swift
+++ RaspberryAlarm/MainView/MainViewController.swift
@@ -12,27 +12,52 @@ import HGCircularSlider
 class MainViewController: UIViewController {
     @IBOutlet weak var hourIndicatingSlider: CircularSlider!
     @IBOutlet weak var minuteIndicatingSlider: CircularSlider!
-    
     @IBOutlet weak var hourLabel: UILabel!
     @IBOutlet weak var minuteLabel: UILabel!
+    let defaultWakeUpHour = 6
+    let defaultWakeUpMinute = 30
+    
+    var wakeUpHour:Int{
+        get{
+            if UserDefaults.standard.object(forKey: wakeUpHourKey) == nil {
+                return defaultWakeUpHour
+            }else{
+                return UserDefaults.standard.integer(forKey: wakeUpHourKey)
+            }
+        }
+    }
+    
+    var wakeUpMinute:Int{
+        get{
+            if UserDefaults.standard.object(forKey: wakeUpMinuteKey) == nil {
+                return defaultWakeUpMinute
+            }else{
+                return UserDefaults.standard.integer(forKey: wakeUpMinuteKey)
+            }
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if UserDefaults.standard.object(forKey: "wakeUpHour") == nil {
-            hourIndicatingSlider.endPointValue = 6.0
-            minuteIndicatingSlider.endPointValue = 30.0
-            hourLabel.text = "06:"
-            minuteLabel.text = "30"
-        }else{
-            hourIndicatingSlider.endPointValue = CGFloat(UserDefaults.standard.integer(forKey: "wakeUpHour"))
-            minuteIndicatingSlider.endPointValue = CGFloat(UserDefaults.standard.integer(forKey: "wakeUpMinute"))
-            hourLabel.text = "\(UserDefaults.standard.integer(forKey: "wakeUpHour")):"
-            minuteLabel.text = "\(UserDefaults.standard.integer(forKey: "wakeUpMinute"))"
-        }
-
-        // Do any additional setup after loading the view.
+        initViews()
     }
-
+    
+    private func initViews(){
+        setUp(hour: wakeUpHour)
+        setUp(minute: wakeUpMinute)
+    }
+    
+    private func setUp(hour:Int){
+        hourIndicatingSlider.endPointValue = CGFloat(hour)
+        hourLabel.text = hour > 10 ? "\(hour):" : "0\(hour):"
+    }
+    
+    private func setUp(minute:Int){
+        minuteIndicatingSlider.endPointValue = CGFloat(minute)
+        minuteLabel.text = minute > 10 ? "\(minute)" : "0\(minute)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -41,23 +66,13 @@ class MainViewController: UIViewController {
     @IBAction func hourChangeHandler(_ sender: CircularSlider) {
         let hour = Int(sender.endPointValue)
         hourLabel.text = hour < 10 ? "0\(hour):" : "\(hour):"
-        UserDefaults.standard.set(hour, forKey: "wakeUpHour")
+        UserDefaults.standard.set(hour, forKey: wakeUpHourKey)
     }
     
     @IBAction func minuteChangeHandler(_ sender: CircularSlider) {
         let minute = Int(sender.endPointValue)
         minuteLabel.text = minute < 10 ? "0\(minute)" : "\(minute)"
-        UserDefaults.standard.set(minute, forKey: "wakeUpMinute")
+        UserDefaults.standard.set(minute, forKey: wakeUpMinuteKey)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ RaspberryAlarm/MainView/UserDefaultsKeys.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  RaspberryAlarm
+//
+//  Created by 류성두 on 2017. 11. 21..
+//  Copyright © 2017년 류성두. All rights reserved.
+//
+
+import Foundation
+
+let wakeUpHourKey = "wakeUpHour"
+let wakeUpMinuteKey = "wakeUpMinute"
