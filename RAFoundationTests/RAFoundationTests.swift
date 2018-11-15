e968e1610eb83ad9c209db110c12d1b5649ceea9
e-sung <dev.esung@gmail.com>
2018-11-15 10:43:10 +0900

    test : add Foundation Scheme and Test

---
+++ RAFoundationTests/RAFoundationTests.swift
@@ -0,0 +1,28 @@
+//
+//  RAFoundationTests.swift
+//  RAFoundationTests
+//
+//  Created by Flitto on 15/11/2018.
+//  Copyright © 2018 류성두. All rights reserved.
+//
+
+import XCTest
+@testable import RAFoundation
+
+class RAFoundationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(1 + 1 == 2)
+        XCTAssert("Hello" + "World" == "HelloWorld")
+    }
+}
