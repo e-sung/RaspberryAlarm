e96263fe8d199c17e4574d9413d1c367b9a9bb84
e-sung <dev.esung@gmail.com>
2018-12-09 15:58:58 +0900

    feat : add HealthKitHelper

---
+++ HealthKitHelper/HealthKitHelper.h
@@ -0,0 +1,19 @@
+//
+//  HealthKitHelper.h
+//  HealthKitHelper
+//
+//  Created by 류성두 on 09/12/2018.
+//  Copyright © 2018 류성두. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HealthKitHelper.
+FOUNDATION_EXPORT double HealthKitHelperVersionNumber;
+
+//! Project version string for HealthKitHelper.
+FOUNDATION_EXPORT const unsigned char HealthKitHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HealthKitHelper/PublicHeader.h>
+
+
