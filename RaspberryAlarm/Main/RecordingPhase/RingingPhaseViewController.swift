43fd0bef18df07b7263dc051dc5a89569b824fe1
e-sung <dev.esung@gmail.com>
2018-12-10 08:29:18 +0900

    fix : ringing phase rings

--- RaspberryAlarm/Main/RecordingPhase/RingingPhaseViewController.swift
+++ RaspberryAlarm/Main/RecordingPhase/RingingPhaseViewController.swift
@@ -16,28 +16,37 @@ import HealthKitHelper
 class RingingPhaseViewController: UIViewController {
 
     var startDate: Date!
-    private var alarmPlayer:AVPlayer {
-        let url = Bundle.main.url(forResource: "alarm", withExtension: "mp3")!
-        return AVPlayer(url: url)
-    }
+    private var alarmPlayer:AVAudioPlayer?
     // MARK: IBActions
     @IBAction private func snoozeButtonHandler(_ sender: UIButton) {
         performSegue(withIdentifier: "unwindToRecordingPhase", sender: nil)
     }
     
     @IBAction private func terminateButtonHandler(_ sender: UIButton) {
-        let turnOffURL = UserDefaults.standard.url(forKey: URLsKeys[2])!
-        URLSession.shared.dataTask(with: turnOffURL).resume()
-        self.dismiss(animated: true, completion: nil)
         HealthKitHelper.shared.saveSleepAnalysis(from: startDate)
+        self.dismiss(animated: true, completion: {
+            guard let turnOffURL = UserDefaults.standard.url(forKey: URLsKeys[2]) else { return }
+            URLSession.shared.dataTask(with: turnOffURL).resume()
+        })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpAlarmPlayer()
+        alarmPlayer?.play()
     }
     
     // MARK: 알람이 울릴 때 행해져야 할 일들
     override func viewDidAppear(_ animated: Bool) {
         UIApplication.shared.isIdleTimerDisabled = true // 핸드폰 안 꺼지게 하는 설정
-        alarmPlayer.play()
-
     }
+    
+    func setUpAlarmPlayer() {
+        let path = Bundle.main.path(forResource: "alarm", ofType: "mp3")
+        let url = URL(fileURLWithPath: path!)
+        alarmPlayer = try? AVAudioPlayer(contentsOf: url)
+    }
+
     
     // MARK: 다시 핸드폰 꺼질 수 있는 상태로 복귀
     override func viewDidDisappear(_ animated: Bool) {
